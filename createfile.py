4c21e8b29dfbbae46b2097b686c3b5ab8be262fa
mak1323 <myleskaeding@gmail.com>
2017-08-15 09:14:56 -0400

    Deleted files for master

--- createfile.py
+++
@@ -1,9 +0,0 @@
-def Main():
-    words = ["cat", "hay", "goat", "roast"]
-
-    with open("words.txt", 'w') as f:
-        for word in words:
-            f.write(word + "\n")
-
-if __name__ == "__main__":
-    Main()
